195bcf4eb5176cae4f2b970185a53b9b2b10dc3e
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2023-07-06 15:57:23 -0500

    chore: hello, tailwind!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,7 @@
-<h1>RANCHO</h1>
+<script>
+	import '../app.css';
+</script>
+
+<h1 class="text-blue-600 italic underline font-bold text-3xl">RANCHO</h1>
 <h2>RANked CHOice [voting]</h2>
 <slot />
-
-<style>
-	h1 {
-		color: blue;
-	}
-</style>
